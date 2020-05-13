883a173fdca308c9a124c73bb44aeb414e1221d1
vykes <knrymcleish@gmail.com>
2020-05-12 21:10:44 -0500

    configure initial dependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')

f23bf9a5bada866aedec288fa6398b360b373bd5
vykes <knrymcleish@gmail.com>
2020-05-13 22:00:13 -0500

    validate form

--- src/components/questions/CreateQuestion.vue
+++ src/components/questions/CreateQuestion.vue
@@ -5,70 +5,75 @@
         Create new Trivia
       </h1>
       <div class="card">
-        <div class="card-header">
-          <input
-            type="name"
-            class="form-control no-border"
-            id="inputName"
-            placeholder="Type a question"
-          />
-        </div>
-        <div class="card-block">
-          <div class="mt-2 ml-3">Options</div>
-          <div class="form-horizontal">
-            <div class="form-group">
-              <div class="col-md-6">
-                <textarea
-                  v-model="optionsInput"
-                  class="form-control"
-                  rows="3"
-                  placeholder="Enter options - press enter to separate"
-                  required
-                ></textarea>
+        <form class="needs-validation" novalidate @submit.prevent="addTrivia">
+          <div class="card-header">
+            <input
+              type="name"
+              class="form-control"
+              id="inputName"
+              placeholder="Type a question"
+              required
+            />
+          </div>
+          <div class="card-block">
+            <div class="mt-2 ml-3">Options</div>
+            <div class="form-horizontal">
+              <div class="form-group">
+                <div class="col-md-6">
+                  <textarea
+                    v-model="optionsInput"
+                    class="form-control"
+                    rows="3"
+                    placeholder="Enter options - press enter to separate"
+                    required
+                  ></textarea>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="btn-group btn-group-toggle ml-3" data-toggle="buttons">
-            <label
-              class="btn btn-outline-secondary btn-toggle"
-              v-for="value in options"
-              v-bind:key="value"
-              @click="answer = value"
-              checked
+            <div class="btn-group btn-group-toggle ml-3" data-toggle="buttons">
+              <label
+                class="btn btn-outline-secondary btn-toggle"
+                v-for="value in options"
+                v-bind:key="value"
+                @click="answer = value"
+              >
+                <input
+                  type="radio"
+                  :id="value"
+                  autocomplete="off"
+                  name="options"
+                />
+                {{ value }}
+              </label>
+            </div>
+            <span v-if="options.length > 1" class="mx-2"
+              >select which is the correct answer</span
             >
-              <input
-                type="radio"
-                :id="value"
-                autocomplete="off"
-                name="options"
-              />
-              {{ value }}
-            </label>
-          </div>
-          <span v-if="options.length > 1" class="mx-2"
-            >select which is the correct answer</span
-          >
-          <div class="mt-2 ml-3">Tags</div>
-          <vue-tags
-            class="ml-3 mt-2"
-            :active="activeTags"
-            :all="allTags"
-            :element-count-for-start-arrow-scrolling="3"
-            :tab-index="1"
-            :colorsEnabled="true"
-            :placeholder="'add tags to this question'"
-            :tagCreationEnabled="true"
-            @on-tag-created="onTagCreated"
-            @on-tag-removed="onTagRemoved"
-            @on-tag-added="onTagAdded"
-          />
+            <div class="mt-2 ml-3">Tags</div>
+            <vue-tags
+              class="ml-3 mt-2"
+              :active="activeTags"
+              :all="allTags"
+              :element-count-for-start-arrow-scrolling="3"
+              :tab-index="1"
+              :colorsEnabled="true"
+              :placeholder="'add tags to this question'"
+              :tagCreationEnabled="true"
+              @on-tag-created="onTagCreated"
+              @on-tag-removed="onTagRemoved"
+              @on-tag-added="onTagAdded"
+            />
+            <div v-if="error" class="invalid-feedback ml-3">
+              please add at least one tag.
+            </div>
 
-          <button class="float-right btn btn-primary mr-4">
-            Submit
-          </button>
-          <br />
-          <br />
-        </div>
+            <button class="float-right btn btn-primary mr-4">
+              Submit
+            </button>
+            <br />
+            <br />
+          </div>
+        </form>
       </div>
     </div>
   </div>
@@ -85,13 +90,13 @@
         answer: '',
         activeTags: [],
         allTags: [],
+        error: false,
       }
     },
     watch: {
       optionsInput: function() {
         const val = this.optionsInput.split(/\r?\n/)
         const rmBlanks = val.filter((e) => e != '')
-        console.log(rmBlanks)
         if (val[0].trim() == '' || val.length < 2) {
           this.options = []
           return
@@ -109,12 +114,33 @@
         tag.id = this.activeTags.length + 1
         this.activeTags.push(tag)
         this.allTags.push(tag)
+        this.error = false
       },
       onTagRemoved(tag) {
         this.activeTags = this.activeTags.filter((t) => t.id != tag.id)
       },
       onTagAdded(val) {
         this.activeTags.push(val)
+        this.error = false
+      },
+      addTrivia() {
+        if (this.invalidForm()) {
+          return
+        }
+        console.log('valid')
+      },
+      invalidForm() {
+        if (this.options.length == 0) {
+          this.optionsInput.trim()
+        }
+        if (this.activeTags.length == 0) {
+          this.error = true
+        }
+        var forms = document.getElementsByClassName('needs-validation')
+        if (forms[0].checkValidity() === false) {
+          forms[0].classList.add('was-validated')
+        }
+        return this.error || forms[0].checkValidity() === false
       },
     },
   }
@@ -154,5 +180,13 @@
   }
   .tags__list {
     width: 100% !important;
+  }
+  .invalid-feedback {
+    /* display: none; */
+    display: block;
+    width: 100%;
+    margin-top: 0.25rem;
+    font-size: 80%;
+    color: #dc3545;
   }
 </style>

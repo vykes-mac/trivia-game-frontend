49fee582dec05c538619931a639e371b306c7c22
vykes-mac <47489215+vykes-mac@users.noreply.github.com>
2020-05-15 22:25:57 -0500

    Update axios_instance.js

--- src/apis/axios_instance.js
+++ src/apis/axios_instance.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = 'https://fast-depths-01924.herokuapp.com/api/v1'
+const API_URL = `process.env.API_ENDPOINT`
 
 export const instance = axios.create({
   baseURL: API_URL,

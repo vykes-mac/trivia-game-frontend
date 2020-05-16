877ac82b8e91a3906121a2b78b9aa655058de819
vykes <knrymcleish@gmail.com>
2020-05-15 20:55:41 -0500

    heroku config

--- src/apis/axios_instance.js
+++ src/apis/axios_instance.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = 'http://localhost:3000/api/v1'
+const API_URL = `${process.env.API_ENDPOINT}`
 export const instance = axios.create({
   baseURL: API_URL,
   headers: { 'Content-Type': 'application/json' },

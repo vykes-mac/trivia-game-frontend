1babb3580e6272f32c4f0c7bff3653d8117aeafb
vykes <knrymcleish@gmail.com>
2020-05-17 17:58:39 -0500

    change static.json to read dist

--- src/components/auth/SignIn.vue
+++ src/components/auth/SignIn.vue
@@ -23,15 +23,9 @@
         required
       />
 
-      <button class="btn btn-lg btn-primary btn-block" type="submit">
-        Sign in
-      </button>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       <router-link to="/signup" class="btn">SignUp</router-link>
-      <div
-        v-if="loading"
-        class="mt-2 spinner-border text-secondary"
-        role="status"
-      >
+      <div v-if="loading" class="mt-2 spinner-border text-secondary" role="status">
         <span class="sr-only">Loading...</span>
       </div>
 
@@ -41,122 +35,122 @@
 </template>
 
 <script>
-  import { signIn } from '@/apis/auth/auth'
-  export default {
-    name: 'signin',
-    data() {
-      return {
-        email: '',
-        password: '',
-        error: '',
-        loading: false,
-      }
-    },
+import { signIn } from "@/apis/auth/auth";
+export default {
+  name: "signin",
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: "",
+      loading: false
+    };
+  },
 
-    methods: {
-      signin() {
-        this.loading = true
-        signIn(this.email, this.password)
-          .then((response) => this.signinSuccessful(response))
-          .catch((error) => console.log(error))
-      },
-      signinSuccessful(response) {
-        this.loading = false
-        if (response.data.error) {
-          this.signinFailure(response.data.error)
-          return
-        }
-        localStorage.token = response.data.auth_token
-        //console.log(localStorage.getItem('token'))
-        //localStorage.signedIn = true
-        this.error = ''
-        this.$router.push({ path: '/home' })
-      },
-      signinFailure(error) {
-        this.error = error.user_authentication
-      },
+  methods: {
+    signin() {
+      this.loading = true;
+      signIn(this.email, this.password)
+        .then(response => this.signinSuccessful(response))
+        .catch(error => console.log(error));
     },
+    signinSuccessful(response) {
+      this.loading = false;
+      if (response.data.error) {
+        this.signinFailure(response.data.error);
+        return;
+      }
+      localStorage.token = response.data.auth_token;
+      console.log(localStorage.getItem("token"));
+      //localStorage.signedIn = true
+      this.error = "";
+      this.$router.push({ path: "/home" });
+    },
+    signinFailure(error) {
+      this.error = error.user_authentication;
+    }
   }
+};
 </script>
 
 <style>
-  .singin-container {
-    height: 100%;
-    *display: -ms-flexbox;
-    display: -webkit-box;
-    display: flex;
-    -ms-flex-align: center;
-    -ms-flex-pack: center;
-    -webkit-box-align: center;
-    align-items: center;
-    -webkit-box-pack: center;
-    justify-content: center;
-    padding-top: 40px;
-    padding-bottom: 40px;
-    text-align: center;
-  }
-  .form-signin {
-    width: 100%;
-    max-width: 330px;
-    padding: 15px;
-    margin: 0 auto;
-  }
-  .form-signin .checkbox {
-    font-weight: 400;
-  }
-  .form-signin .form-control {
-    position: relative;
-    box-sizing: border-box;
-    height: auto;
-    padding: 10px;
-    font-size: 16px;
-  }
-  .form-signin .form-control:focus {
-    z-index: 2;
-  }
-  .form-signin input[type='email'] {
-    margin-bottom: -1px;
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-  }
-  .form-signin input[type='password'] {
-    margin-bottom: 10px;
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-  }
+.singin-container {
+  height: 100%;
+  *display: -ms-flexbox;
+  display: -webkit-box;
+  display: flex;
+  -ms-flex-align: center;
+  -ms-flex-pack: center;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  text-align: center;
+}
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
 
-  .btn-primary {
-    color: #fff;
-    background-color: #24e0ae;
-    border-color: #24e0ae;
-  }
+.btn-primary {
+  color: #fff;
+  background-color: #24e0ae;
+  border-color: #24e0ae;
+}
 
-  .btn-primary:hover {
-    color: #fff;
-    background-color: #1ebd93;
-    border-color: #1ebd93;
-  }
+.btn-primary:hover {
+  color: #fff;
+  background-color: #1ebd93;
+  border-color: #1ebd93;
+}
 
-  .form-control:focus {
-    color: #24e0ae;
-    background-color: #fff;
-    border-color: #24e0ae !important;
-    outline: 0;
-    box-shadow: 0 0 0 0.2rem rgba(41, 255, 198, 0.25);
-  }
+.form-control:focus {
+  color: #24e0ae;
+  background-color: #fff;
+  border-color: #24e0ae !important;
+  outline: 0;
+  box-shadow: 0 0 0 0.2rem rgba(41, 255, 198, 0.25);
+}
 
-  .btn-primary:focus {
-    color: #fff;
-    background-color: #1ebd93;
-    border: none;
-    box-shadow: none;
-  }
+.btn-primary:focus {
+  color: #fff;
+  background-color: #1ebd93;
+  border: none;
+  box-shadow: none;
+}
 
-  .btn-primary:not(:disabled):not(.disabled):active {
-    color: #fff;
-    background-color: #1ebd93;
-    border-color: none;
-    border: none;
-    box-shadow: none;
-  }
+.btn-primary:not(:disabled):not(.disabled):active {
+  color: #fff;
+  background-color: #1ebd93;
+  border-color: none;
+  border: none;
+  box-shadow: none;
+}
 </style>

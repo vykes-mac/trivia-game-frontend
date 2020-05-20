70acf7067eab2c77d1132c7c7eefab83b188572f
vykes <knrymcleish@gmail.com>
2020-05-19 22:11:32 -0500

    fix axios headers to get token on each request

--- src/apis/axios_instance.js
+++ src/apis/axios_instance.js
@@ -1,12 +1,20 @@
 import axios from 'axios'
-const API_URL = `process.env.API_ENDPOINT`
+const API_URL = 'https://fast-depths-01924.herokuapp.com/api/v1'
 
-export const instance = axios.create({
+const instance = axios.create({
   baseURL: API_URL,
   headers: { 'Content-Type': 'application/json' },
 })
 
-export const secureInstance = axios.create({
+const secureInstance = axios.create({
   baseURL: API_URL,
-  headers: { Authorization: localStorage.getItem('token') },
 })
+
+secureInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  config.headers.Authorization = token
+
+  return config
+})
+
+export { instance, secureInstance }
